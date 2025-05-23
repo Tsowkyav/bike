96230d9cc4d64c95c95ae959fccc84361c670cc2
Tsowkyav <tsowkyavi59@gmail.com>
2025-05-23 17:41:01 +0530

    Update bikess.py

--- bikess.py
+++ bikess.py
@@ -1,21 +1,4 @@
-'''print(50+4)
-a=int(input ("enter a number:"))
-if a %2==0:
-    print("number is even")
-else:
-    print("number is odd")'''
-'''age=int(input("enter a number"))
-if age >=18:
-    print("eligible")
-else:
-    print("not eligible ")
-num=int(input("enter a number:"))
-if num%7==0:
-    print("the number is divisival by seven")
-elif num%7>0:
-    print("not divisile by seven")
-else:
-    print("not")'''
+
 class bike1():
     def person1(self, button):
         self. button= button
